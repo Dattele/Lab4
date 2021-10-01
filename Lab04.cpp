e7c5d0080b1c2494f0e3c5bc03318f82b3d1e3b2
baumbelc <baumbelc@ceclnx01-19.csi.miamioh.edu>
2021-10-01 13:30:40 -0400

    assignment

--- Lab04.cpp
+++ Lab04.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 
 void Q1() {
         std::cout << "This is the code for the first question";
@@ -22,15 +23,34 @@ int Q2() {
  	return average;
 }
 
-// int Q3() {
+void Q3() {
+	std::unordered_map<std::string, int> map;
 
-// }
+	std::ifstream inFile("NameFile.txt");
+	std::ifstream gradeFile("input.txt");
+
+	std:string name;
+	int num;
+	while ((inFile >> name && gradeFile >> num)) {
+		map.insert(std::pair<std::string, int> {name, num});
+	}
+
+	std::string key;
+	std::cin >> key;
+	
+	if (map.find(key) == map.end()) {
+		std::cout << "Not exists";
+	} else {
+		std::cout << map[key];
+	}
+}
 
 int main(int argc, char *argv[]) {
         switch(argv[1][1]) {
                 case '1': Q1(); break;
                 case '2': Q2(); break;
-        }
+ 		case '3': Q3(); break;
+ 	}
 
         return 0;
 }
